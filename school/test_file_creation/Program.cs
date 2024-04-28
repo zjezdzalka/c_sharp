2449c091e25548769b67ab0ffb97d0bc6e03c30f
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-04-28 20:51:33 +0200

    choice
    asd

---
+++ school/test_file_creation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace test_file_creation
+{
+    internal class Program
+    {
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream(@"c:\xampp\htdocs\c_sharp\school\test_file_creation\asd.txt", FileMode.OpenOrCreate);
+            AddText(file, "asd");
+            file.Close();
+            Console.ReadKey();
+        }
+    }
+}
